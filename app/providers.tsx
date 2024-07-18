06331920856f7102e7274b7ce446f2edff4e0c44
rizkyRosyidHidayat <ocid.271198@gmail.com>
2024-07-18 16:39:31 +0700

    add color primary

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,25 @@
-'use client'
+"use client";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      "50": "#fef6ee",
+      "100": "#fcebd8",
+      "200": "#f9d2af",
+      "300": "#f4b27d",
+      "400": "#ef8848",
+      "500": "#ea6723",
+      "600": "#dc501a",
+      "700": "#b63b18",
+      "800": "#91301b",
+      "900": "#752a19",
+      "950": "#3f120b",
+    },
+  },
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
