da1abb0342f383c333c80cb41c28d00e5e0f5bdf
rizkyRosyidHidayat <ocid.271198@gmail.com>
2024-07-18 15:09:35 +0700

    integrated chakra ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dmSans.className}>
+        <Providers>
+          <section>{children}</section>
+        </Providers>
+      </body>
     </html>
   );
 }
