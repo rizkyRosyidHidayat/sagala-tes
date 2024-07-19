84b5096ddf21822d7344c200e809cf14e5d03972
rizkyRosyidHidayat <ocid.271198@gmail.com>
2024-07-19 21:13:56 +0700

    add check table component

---
+++ components/CheckTable.tsx
@@ -0,0 +1,303 @@
+"use client";
+import { formatDate, getRandomInt } from "@/utils/helper";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Circle,
+  Flex,
+  GridItem,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  SimpleGrid,
+  Spacer,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { PureComponent } from "react";
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
+
+const tableHead = ["NAME", "PROGRESS", "QUANTITY", "DATE"];
+type DataTableType = {
+  name: string;
+  progress: string;
+  quantity: number;
+  date: string;
+};
+const dataTable: DataTableType[] = [
+  {
+    name: "Fried Rice",
+    progress: getRandomInt(100) + "%",
+    quantity: getRandomInt(2000),
+    date: "2024-05-20",
+  },
+  {
+    name: "Coffe Capuchino",
+    progress: getRandomInt(100) + "%",
+    quantity: getRandomInt(2000),
+    date: "2024-07-19",
+  },
+  {
+    name: "Hot Wings Chinken",
+    progress: getRandomInt(100) + "%",
+    quantity: getRandomInt(2000),
+    date: "2024-04-23",
+  },
+  {
+    name: "Thai Tea Boba",
+    progress: getRandomInt(100) + "%",
+    quantity: getRandomInt(2000),
+    date: "2024-11-22",
+  },
+  {
+    name: "Matcha Original",
+    progress: getRandomInt(100) + "%",
+    quantity: getRandomInt(2000),
+    date: "2024-01-09",
+  },
+];
+
+const dataVisitors = [
+  { name: "00", uv: getRandomInt(400) },
+  { name: "04", uv: getRandomInt(400) },
+  { name: "08", uv: getRandomInt(400) },
+  { name: "12", uv: getRandomInt(400) },
+  { name: "14", uv: getRandomInt(400) },
+  { name: "16", uv: getRandomInt(400) },
+  { name: "18", uv: getRandomInt(400) },
+];
+
+const dataPie = [
+  { name: "Group A", value: getRandomInt(400) },
+  { name: "Group B", value: getRandomInt(400) },
+  { name: "Group C", value: getRandomInt(400) },
+];
+
+const COLORS = ["#8884D8", "#82CA9D", "#0098e5"];
+
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const { x, y, stroke, payload }: any = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text
+          x={0}
+          y={0}
+          dy={16}
+          fontSize={14}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="#A0AEC0"
+        >
+          {payload.value}
+        </text>
+      </g>
+    );
+  }
+}
+
+
+export default function CheckTable() {
+  return (
+    <SimpleGrid columns={4} gap="20px">
+      <Box as={GridItem} colSpan={2}>
+        <Card variant="outline" borderRadius="24px" border="0px">
+          <CardHeader as={Flex} alignItems="center">
+            <Heading size="md" color="primary.900">
+              Check Table
+            </Heading>
+            <Spacer />
+            <InputGroup width={300} variant="pill">
+              <InputLeftElement pointerEvents="none">
+                <MagnifyingGlassIcon className="size-5 text-gray-500" />
+              </InputLeftElement>
+              <Input type="text" placeholder="Search data" />
+            </InputGroup>
+          </CardHeader>
+          <CardBody pt={0} px={0}>
+            <Table>
+              <Thead>
+                <Tr>
+                  {tableHead.map((text, headKey) => (
+                    <Th key={headKey} color="gray.400">
+                      {text}
+                    </Th>
+                  ))}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {dataTable.map((data, dataKey) => (
+                  <Tr key={data.name}>
+                    <Td border={0} fontSize="sm">
+                      {data.name}
+                    </Td>
+                    <Td border={0} fontSize="sm">
+                      {data.progress}
+                    </Td>
+                    <Td border={0} fontSize="sm">
+                      {data.quantity}
+                    </Td>
+                    <Td border={0} fontSize="sm">
+                      {formatDate(data.date)}
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      </Box>
+      <Box as={GridItem} colSpan={1}>
+        <Card variant="outline" borderRadius="24px" border="0px" height="100%">
+          <CardBody>
+            <Flex
+              flexDirection="column"
+              height="100%"
+              justifyContent="space-between"
+            >
+              <Stat>
+                <Flex alignItems="center">
+                  <StatLabel color="gray.400">Daily Traffic</StatLabel>
+                  <Spacer />
+                  <StatHelpText mb={0} as="b" color="green.400">
+                    <StatArrow type="increase" />
+                    23.36%
+                  </StatHelpText>
+                </Flex>
+                <Flex alignItems="center">
+                  <StatLabel color="gray.400">Visitors</StatLabel>
+                  <Spacer />
+                  <StatNumber>3.454</StatNumber>
+                </Flex>
+              </Stat>
+              <Spacer />
+              <ResponsiveContainer width="100%" height={200}>
+                <BarChart data={dataVisitors}>
+                  <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="0%" stopColor="#8884D8" stopOpacity={1} />
+                      <stop offset="100%" stopColor="#82CA9D" stopOpacity={1} />
+                    </linearGradient>
+                  </defs>
+                  <XAxis
+                    dataKey="name"
+                    axisLine={false}
+                    tickLine={false}
+                    tick={<CustomizedAxisTick />}
+                    scale="point"
+                  />
+                  <YAxis tick={false} axisLine={false} width={10} />
+                  <Tooltip />
+                  <Bar barSize={20} dataKey="uv" fill="url(#colorUv)" />
+                </BarChart>
+              </ResponsiveContainer>
+            </Flex>
+          </CardBody>
+        </Card>
+      </Box>
+      <Box as={GridItem} colSpan={1}>
+        <Card variant="outline" borderRadius="24px" border="0px" height="100%">
+          <CardBody>
+            <Flex alignItems="center">
+              <Heading color="primary.900" size="sm">
+                Your Pie Chart
+              </Heading>
+              <Spacer />
+              <Menu>
+                <MenuButton
+                  as={Flex}
+                  fontSize="14"
+                  color="gray.400"
+                  cursor="pointer"
+                >
+                  <Text as="span" mr="4px">
+                    Monthly
+                  </Text>
+                  <ChevronDownIcon />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem color="gray.400" fontSize="14px">
+                    Monthly
+                  </MenuItem>
+                  <MenuItem color="gray.400" fontSize="14px">
+                    Weekly
+                  </MenuItem>
+                  <MenuItem color="gray.400" fontSize="14px">
+                    Daily
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+            <PieChart width={250} height={250}>
+              <Pie
+                data={dataPie}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {dataPie.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </CardBody>
+          <CardFooter justifyContent="space-between">
+            {dataPie.map((data, key) => (
+              <Box key={key}>
+                <Flex alignItems="center" gap="8px">
+                  <span
+                    style={{ background: COLORS[key] }}
+                    className={`w-2 h-2 inline-block rounded-full`}
+                  ></span>
+                  <Text color="gray.400" fontSize="sm">
+                    {data.name}
+                  </Text>
+                </Flex>
+                <Text as="b" fontSize="lg">
+                  {data.value}
+                </Text>
+              </Box>
+            ))}
+          </CardFooter>
+        </Card>
+      </Box>
+    </SimpleGrid>
+  );
+}
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,3 +1,11 @@
 export function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("id-ID", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })
+}
